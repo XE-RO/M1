17abaab15af1fa7a692ec999a24aed07d14e9928
XE-RO <delgadox0@gmail.com>
2023-09-06 12:33:05 -0600

    H7 check

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let factors = [1]
+  let x = 2
 
+  while(num>1){
+    if(num % x === 0){
+      factors.push(x)
+      num=num/x
+    }else{
+      x++
+    }
+  }
+  return factors
 }
 
 function bubbleSort(array) {
@@ -14,6 +25,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i=0 ; i<array.length-1 ; i++){
+    for(let j=0 ; j<array.length-i-1 ; j++){
+      if(array[j]>array[j+1]){
+        let aux=array[j]
+        array[j]=array[j+1]
+        array[j+1]=aux
+      }
+    }
+  }
+  return array
 
 }
 
@@ -23,7 +44,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i=1 ; i<array.length ; i++){
+    let j = i-1
+    let aux=array[i]
+    while(j>=0 && aux < array[j]){
+      array[j+1]=array[j]
+      j--
+    }
+    array[j+1]=aux
+  }
+  return array
 }
 
 
@@ -32,7 +62,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i=0 ; i<array.length ; i++){
+    let min = i
 
+    for(let j=i+1 ; j<array.length ; j++){
+      if(array[j]<array[min]){
+        min=j
+      }
+    }
+    if(i!==min){
+      let aux=array[i]
+      array[i]=array[min]
+      array[min]=aux
+    }
+  }
+  return array
 }
 
 
