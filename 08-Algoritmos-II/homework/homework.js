9bf7a3cd4a1315258ce8dfdd7e2a6177f57dd777
XE-RO <delgadox0@gmail.com>
2023-09-07 18:56:06 -0600

    H8 check

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,6 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length<=1)return array
+  
+  let random=Math.floor(Math.random()*array.length)
+  let pivot=array[random]
+  let equals=[]
+  let left=[]
+  let right=[]
+  for(let i=0 ; i<array.length ; i++){
+    if(array[i]!==pivot){
+      if(array[i]<pivot)left.push(array[i])
+      else right.push(array[i])
+    }
+    else equals.push(array[i])
+  }
+  return quickSort(left).concat(equals).concat(quickSort(right))
+
 
 }
 
@@ -16,6 +32,33 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+  
+  if(array.length < 2) return array;
+
+  let mitad = Math.floor(array.length / 2)
+  let left = array.slice(0,mitad)
+  let right = array.slice(mitad)
+  
+  let arrayNuevo =  []
+  
+  left = mergeSort(left)
+  right= mergeSort(right)
+  
+  while(left.length && right.length){
+    if(left[0] < right[0]){
+      arrayNuevo.push(left.shift())
+    } 
+    else arrayNuevo.push(right.shift())
+  
+  }
+    while(left.length){
+      arrayNuevo.push(left.shift())
+    }
+    while(right.length) {
+      arrayNuevo.push(right.shift())
+    }
+    return arrayNuevo
+
 }
 
 // No modificar nada debajo de esta línea
